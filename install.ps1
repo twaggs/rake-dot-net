a8244815053bd71b86e82379b6820fa48cb2976a
Amir Rajan <ar@amirrajan.net>
2012-11-18 19:48:28 -0600

    added round robin load balance simulation

--- install.ps1
+++ install.ps1
@@ -9,9 +9,13 @@ $rakeFileTo = "Rakefile.rb"
 $devYmlFrom = $toolsPath + "\..\src\dev.yml"
 $devYmlTo = "dev.yml"
 
+$nginxFrom = $toolsPath + "\..\src\nginx"
+$nginxTo = "nginx"
+
 if(!(Test-Path $rakeFileTo))
 {
   Copy-Item $rakeDotNetFrom $rakeDotNetTo -recurse
+  Copy-Item $nginxFrom $nginxTo -recurse
   Copy-Item $rakeFileFrom $rakeFileTo
   Copy-Item $devYmlFrom $devYmlTo
 }
